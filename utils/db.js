0a8eb92673f0ef95e565bfb339cca8d599af7b64
Emmanuel-Mbaeze <mbaezesomto@gmail.com>
2022-06-06 10:17:53 -0700

    Socialpush

---
+++ utils/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const url = process.env.URL;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("database is now connected...!");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+// const mongoose = require("mongoose");
+// const url = "mongodb://localhost/DiaryDB";
+// mongoose
+//   .connect(url)
+//   .then(() => {
+//     console.log("Connected to database");
+//   })
+//   .catch(() => {
+//     console.log("Connection failed");
+//   });
